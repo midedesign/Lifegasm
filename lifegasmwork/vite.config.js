23c007a3aa939c30c965b2910c9279be2d457a1b
midedesign <justmidedesign@gmail.com>
2025-09-30 14:20:55 +0100

    Fix

--- lifegasmwork/vite.config.js
+++ lifegasmwork/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  react()],
+  plugins: [tailwindcss(), react()],
+  base: process.env.VITE_BASE_PATH || "/Lifegasm",
 });
